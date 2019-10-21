aaafb4932a17a711f81182a836e01dc1277df9d6
Goran Knezevic <goran.knezevic@kolotree.com>
2019-10-21 19:56:42 +0200

    Added fields in Product and ProductBuilder. Tests failing. WIP

--- src/main/java/com/roche/assignment/model/Product.java
+++ src/main/java/com/roche/assignment/model/Product.java
@@ -1,4 +1,23 @@
 package com.roche.assignment.model;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Product {
+
+    private String sku;
+    private String name;
+    private float price;
+    private LocalDateTime createdOn;
+
+    Product(String name, float price) {
+        this(UUID.randomUUID().toString(), name, price);
+    }
+
+    Product(String sku, String name, float price) {
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+        this.createdOn = LocalDateTime.now();
+    }
 }
--- src/main/java/com/roche/assignment/model/ProductBuilder.java
+++ src/main/java/com/roche/assignment/model/ProductBuilder.java
@@ -1,6 +1,12 @@
 package com.roche.assignment.model;
 
+import java.util.Optional;
+
 public class ProductBuilder {
+
+    private Optional<String> skuOpt = Optional.empty();
+    private Optional<String> nameOpt = Optional.empty();
+    private Optional<Float> priceOpt = Optional.empty();
 
     private ProductBuilder() {}
 
@@ -9,6 +15,8 @@ public class ProductBuilder {
     }
 
     public Product build() {
-        return new Product();
+        return skuOpt
+                .map(sku -> new Product(sku, nameOpt.get(), priceOpt.get()))
+                .orElse(new Product(nameOpt.get(), priceOpt.get()));
     }
 }
